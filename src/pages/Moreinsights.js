989d977301296d5f00bd8ce6cb379f198496b72b
Abhiram krishna21 <abhiramkrishna328@gmail.com>
2024-07-20 14:03:21 +0530

    insights commit

--- src/pages/Moreinsights.js
+++ src/pages/Moreinsights.js
@@ -1,8 +1,84 @@
 import React from 'react'
+import './Moreinsights.css'
 
 function Moreinsights() {
   return (
-    <div>Moreinsights</div>
+    <div>
+      <p className='p1'>INSIGHTS</p>
+      <h1 className='h1'>Perspectives on <br></br> digital and web.</h1>
+      <p className='p2'>Our thoughts on creativity, <br></br>digital, and branding.</p>
+      <div className="insights-grid">
+        <div className="insight">
+          <img className="photo" src="https://i.postimg.cc/cLhWTxPs/1.jpg" alt="Insight 1" />
+          <h4 className="category">WEB & DIGITAL DESIGN</h4>
+          <p className="description">Website Design Briefs: A Guide to <br /> Writing a Creative RFP for <br /> Agencies</p>
+        </div>
+
+        <div className="insight">
+          <img className="photo" src="https://i.postimg.cc/yxB3pSgy/2.jpg" alt="Insight 2" />
+          <h4 className="category">WEB & DIGITAL DESIGN</h4>
+          <p className="description">Beyond the Click: Crafting B2B <br /> Websites That Convert</p>
+        </div>
+
+        <div className="insight">
+          <img className="photo" src="https://i.postimg.cc/wv382nm3/3.jpg" alt="Insight 3" />
+          <h4 className="category">WEB & DIGITAL DESIGN</h4>
+          <p className="description">Driving Engagement On <br /> Your Corporate Website</p>
+        </div>
+
+        <div className="insight">
+          <img className="photo" src="https://i.postimg.cc/4NR44Nc5/4.jpg" alt="Insight 4" />
+          <h4 className="category">WEB & DIGITAL DESIGN</h4>
+          <p className="description">Crafting a Memorable Web <br /> Experience for Your Users</p>
+        </div>
+        <div className="insight">
+          <img className="photo" src="https://i.postimg.cc/cLhWTxPs/1.jpg" alt="Insight 1" />
+          <h4 className="category">WEB & DIGITAL DESIGN</h4>
+          <p className="description">Website Design Briefs: A Guide to <br /> Writing a Creative RFP for <br /> Agencies</p>
+        </div>
+
+        <div className="insight">
+          <img className="photo" src="https://i.postimg.cc/yxB3pSgy/2.jpg" alt="Insight 2" />
+          <h4 className="category">WEB & DIGITAL DESIGN</h4>
+          <p className="description">Beyond the Click: Crafting B2B <br /> Websites That Convert</p>
+        </div>
+
+        <div className="insight">
+          <img className="photo" src="https://i.postimg.cc/wv382nm3/3.jpg" alt="Insight 3" />
+          <h4 className="category">WEB & DIGITAL DESIGN</h4>
+          <p className="description">Driving Engagement On <br /> Your Corporate Website</p>
+        </div>
+
+        <div className="insight">
+          <img className="photo" src="https://i.postimg.cc/4NR44Nc5/4.jpg" alt="Insight 4" />
+          <h4 className="category">WEB & DIGITAL DESIGN</h4>
+          <p className="description">Crafting a Memorable Web <br /> Experience for Your Users</p>
+        </div>
+        <div className="insight">
+          <img className="photo" src="https://i.postimg.cc/cLhWTxPs/1.jpg" alt="Insight 1" />
+          <h4 className="category">WEB & DIGITAL DESIGN</h4>
+          <p className="description">Website Design Briefs: A Guide to <br /> Writing a Creative RFP for <br /> Agencies</p>
+        </div>
+
+        <div className="insight">
+          <img className="photo" src="https://i.postimg.cc/yxB3pSgy/2.jpg" alt="Insight 2" />
+          <h4 className="category">WEB & DIGITAL DESIGN</h4>
+          <p className="description">Beyond the Click: Crafting B2B <br /> Websites That Convert</p>
+        </div>
+
+        <div className="insight">
+          <img className="photo" src="https://i.postimg.cc/wv382nm3/3.jpg" alt="Insight 3" />
+          <h4 className="category">WEB & DIGITAL DESIGN</h4>
+          <p className="description">Driving Engagement On <br /> Your Corporate Website</p>
+        </div>
+
+        <div className="insight">
+          <img className="photo" src="https://i.postimg.cc/4NR44Nc5/4.jpg" alt="Insight 4" />
+          <h4 className="category">WEB & DIGITAL DESIGN</h4>
+          <p className="description">Crafting a Memorable Web <br /> Experience for Your Users</p>
+        </div>
+      </div>
+    </div>
   )
 }
 
